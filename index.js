c98812ab823590c7a69f659f4d74ef151e5530de
Fredrik Eilertsen <Fredrik Eilertsen>
2016-04-14 20:52:12 +0200

    Added semicolons. New dependency: moment.

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var PORT = process.env.PORT || 3000
-var path = require('path')
+var path = require('path');
+var date = new Date();
 
-
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.html'))
-})
-
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
 
 app.use(function(req, res, next) {
-res.setHeader('Content-type', 'text/plain')
-res.status(404).sendFile('index.html')
-console.log('404')
-})
-
+res.status(404)
+res.sendFile(path.join(__dirname, 'index.html'));
+console.log(`Error 404. \n${date}`);
+});
 
 app.listen(PORT, ()=> {
-  console.log(`Server online at port ${PORT}`)
-})
+  console.log(`Server online at port ${PORT}`);
+});
