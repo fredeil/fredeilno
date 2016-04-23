83046cc0a32ec37ed061d38c52c63b2bd8a006ee
Fredrik Eilertsen <Fredrik Eilertsen>
2016-04-23 14:54:43 +0200

    Added support for digitalocean API

--- server.js
+++ server.js
@@ -4,26 +4,33 @@ var app = express();
 var PORT = process.env.PORT || 3000
 var path = require('path');
 
+// Set norwegian local time
 moment.locale("nb");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// route index file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/about', (req, res) => {
+// view  beta site
+app.get('/ncs', (req, res) => {
   res.sendFile(path.join(__dirname, 'about.html'));
 });
 
-app.use(function(req, res, next) {
-res.status(404)
-res.sendFile(path.join(__dirname, 'index.html'));
 
-var date = moment.tz("Europe/Oslo").format("LLL")
+// Error 404. Send back to index file.
+app.use(function(req, res, next) {
+res.status(404);
+res.sendFile(path.join(__dirname, 'index.html'));
+var date = moment.tz("Europe/Oslo").format("LLL"); // Send norwegian timestamp to server located in germany
 console.log(`Error 404. \n${date}\n`);
 });
 
+
+// Console log
 app.listen(PORT, ()=> {
   console.log(`Server online at port ${PORT}`);
 });
