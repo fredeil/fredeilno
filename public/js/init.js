5c15492e378919b13e25bcb9e272c2324ecf97fe
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2017-11-27 10:27:40 +0100

    Delete SASS files

--- public/js/init.js
+++ public/js/init.js
@@ -1,25 +1,41 @@
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
-			'wide': { range: '-1680', href: 'css/style-wide.css' },
-			'normal': { range: '-1280', href: 'css/style-normal.css' },
-			'mobile': { range: '-640', href: 'css/style-mobile.css' },
-			'mobilep': { range: '-360', href: 'css/style-mobilep.css' }
+skel.init({
+	reset: 'full',
+	breakpoints: {
+		'global': {
+			range: '*',
+			href: 'css/style.css',
+			lockViewport: true,
+			viewport: 'minimal-ui'
+		},
+		'wide': {
+			range: '-1680',
+			href: 'css/style-wide.css'
+		},
+		'normal': {
+			range: '-1280',
+			href: 'css/style-normal.css'
+		},
+		'mobile': {
+			range: '-640',
+			href: 'css/style-mobile.css'
+		},
+		'mobilep': {
+			range: '-360',
+			href: 'css/style-mobilep.css'
 		}
-	});
+	}
+});
 
 // JavaScript Events
-	// Fjerne "loading" class når siden er ferdig loaded
-		window.onload = function() {
-			document.body.className = '';
-		}
+window.onload = () => {
+	document.body.className = '';
+}
 
-	// Hindre scrolling på touchskjermer
-		window.ontouchmove = function() {
-			return false;
-		}
+// Hindre scrolling på touchskjermer
+window.ontouchmove = () => {
+	return false;
+}
 
-		window.onorientationchange = function() {
-			document.body.scrollTop = 0;
-		}
+window.onorientationchange = () => {
+	document.body.scrollTop = 0;
+}
